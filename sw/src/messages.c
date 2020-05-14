a9cf7082951e228a84d26e7dac8fcd46ab36f288
Olli Usenius <olli.usenius@usevolt.fi>
2020-05-14 22:06:44 +0300

    cdd adc value added to stat

--- sw/src/messages.c
+++ sw/src/messages.c
@@ -419,7 +419,7 @@ void stat_callb(void* me, unsigned int cmd, unsigned int args, argument_st *argv
 	stat_output(&this->alt_ig, this->alt_ig_enabled, "Alt IG");
 	stat_output(&this->oilcooler, this->oilcooler_enabled, "OilC");
 	stat_output(&this->radiator, this->radiator_enabled, "Radiator");
-	printf("Vdd: %u mV\n", this->vdd);
+	printf("Vdd: %u mV 0x%x\n", this->vdd, uv_adc_read(VDD_SENSE_AIN));
 	printf("Hours: %u\n", (unsigned int) this->hour_counter);
 	printf("FSB ignkey state: %u, emcy: %u\n", this->fsb.ignkey_state, this->fsb.emcy);
 	printf("CSB ac req: %u\n", this->csb.ac_req);
